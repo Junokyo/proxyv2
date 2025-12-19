56a186815ddf938ba780cd540ed30b9cf63c8a3d
Fhuu <jngkao@gmail.com>
2025-12-19 16:05:34 +0700

    chore: rebuild

--- src/sections/account/AccountView.tsx
+++ src/sections/account/AccountView.tsx
@@ -29,7 +29,7 @@ export default function AccountView() {
         {/* Header */}
         <div className="flex flex-col gap-2">
           <h1 className="text-lg font-semibold text-slate-900">
-            Metronic wallet
+            Metronic account
           </h1>
 
           {/* Divider */}
